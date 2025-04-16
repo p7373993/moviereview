b72e0c3cc173fbf9a1701a8f39a5323c36cf72ee
jinhi-112 <seojin072023@gmail.com>
2025-04-16 22:01:38 +0900

    Revert "Revert "Revert "초기"""
    This reverts commit b5de0e4eefac290b0e416e4ee9fdb6f0b0c07ceb.

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+# config/urls.py (또는 프로젝트의 최상위 urls.py)
 from django.contrib import admin
 from django.urls import include, path
 
